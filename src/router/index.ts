f47bc56ee3a7c938af649d20988db86c53df71be
i.shunyaev <i.shunyaev@fact.digital>
2023-09-14 11:38:56 +0400

    [refactor] changed router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,23 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import PeopleView from "../views/PeopleView.vue";
+import FavoritesView from "../views/FavoritesView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: "/people",
+    name: "people",
+    component: PeopleView,
+  },
+  {
+    path: "/favorites",
+    name: "favorites",
+    component: FavoritesView,
   },
 ];
 
