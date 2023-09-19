411527702d446cc9a5aea0af208ab5295fe736d0
i.shunyaev <i.shunyaev@fact.digital>
2023-09-19 12:22:08 +0400

    [feat] added favorite button

---
+++ src/components/favorite/FavoriteButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    :class="['favorite-button', { 'favorite-button_active': active }]"
+    @click="$emit('click')"
+  >
+    <FavoriteIcon />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from "vue";
+import FavoriteIcon from "./FavoriteIcon.vue";
+
+defineEmits(["click"]);
+
+const props = defineProps<{
+  active: boolean;
+}>();
+
+const { active } = toRefs(props);
+</script>
+
+<style lang="scss" scoped>
+.favorite-button {
+  width: 30px;
+  height: 30px;
+  margin: 0;
+  padding: 0;
+  border: none;
+  background: transparent;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+    svg {
+      fill: red;
+    }
+  }
+
+  svg {
+    width: 30px;
+    height: 30px;
+    fill: $lilac-back;
+  }
+
+  &_active {
+    svg {
+      fill: red;
+    }
+  }
+}
+</style>
