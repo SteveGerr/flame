c9425481a5fed5b830e56c32ccdb3fdb9c226768
i.shunyaev <i.shunyaev@fact.digital>
2023-09-19 11:49:09 +0400

    [feat] added interfaces

---
+++ src/interfaces/IPeople.ts
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+export interface IPerson {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: [];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface IPersonFiltered {
+  id: number;
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  url: string;
+  favorite: boolean;
+}
