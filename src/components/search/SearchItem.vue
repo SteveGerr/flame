083cada25bfa27cabf25ec5cb32b386652155166
i.shunyaev <i.shunyaev@fact.digital>
2023-09-19 12:24:33 +0400

    [feat] added search item

---
+++ src/components/search/SearchItem.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
+<template>
+  <li class="search-item">
+    <RouterLink :to="cutUrl(item.url)">
+      <p class="search-item__name">
+        {{ item.name }}
+      </p>
+    </RouterLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+import { IPerson } from "@/interfaces/IPeople";
+
+defineProps<{
+  item: IPerson;
+}>();
+
+const cutUrl = (url: string) => url.replace("https://swapi.dev/api/", "");
+</script>
+
+<style lang="scss" scoped>
+.search-item {
+  padding: 3px;
+}
+</style>
