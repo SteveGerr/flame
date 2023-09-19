1a87a2cdf9152aa87f8568ebd3d01920eec82ad4
i.shunyaev <i.shunyaev@fact.digital>
2023-09-19 11:47:55 +0400

    [feat] added api

--- src/api/peopleApi.ts
+++ src/api/peopleApi.ts
@@ -6,4 +6,12 @@ export default {
   getAllPeople() {
     return API(url).get("people");
   },
+
+  getCharacter(id: string) {
+    return API(url).get(`people/${id}`);
+  },
+
+  findCharacters(value: string) {
+    return API(url).get(`people/?search=${value || null}`);
+  },
 };
