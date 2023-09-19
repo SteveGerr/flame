2cc658f8dca6321772eac66ca1e00ea826e7142a
i.shunyaev <i.shunyaev@fact.digital>
2023-09-19 13:38:58 +0400

    [refactor] removed import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
 import PeopleView from "@/views/PeopleView.vue";
 import FavoritesView from "@/views/FavoritesView.vue";
 import CharacterView from "@/views/CharacterView.vue";
